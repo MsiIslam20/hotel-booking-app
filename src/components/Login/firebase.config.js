6741068f5636c747434b40d376fd79f89a266f0b
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-09-30 11:52:09 +0600

    All file uploaded

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDhiGZWBAiKqx6BMPBEYRV0DGGvKBdQO2Y",
+    authDomain: "hotel-booking-app-61229.firebaseapp.com",
+    databaseURL: "https://hotel-booking-app-61229.firebaseio.com",
+    projectId: "hotel-booking-app-61229",
+    storageBucket: "hotel-booking-app-61229.appspot.com",
+    messagingSenderId: "945290906098",
+    appId: "1:945290906098:web:bbdae582eca0262cc4c36b"
+};
+export default firebaseConfig;
