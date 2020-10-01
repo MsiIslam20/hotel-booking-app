7b86c63524ceb1757ffd5c318700c720346da3f6
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-10-01 19:45:34 +0600

    updates

--- src/components/Bookings/Bookings.js
+++ src/components/Bookings/Bookings.js
@@ -20,7 +20,7 @@ const Bookings = () => {
   
     return (
         <div>
-            <h2>You Have {bookings.length} Bookings!</h2>
+            <h2>You Have {bookings.length} Bookings!!!</h2>
             {
                 bookings.map(book => <li key={book._id}>{book.name} from: {new Date(book.checkIn).toDateString('dd/MM/yyyy')} to: {new Date(book.checkOut).toDateString('dd/MM/yyyy')}</li>)
             }
